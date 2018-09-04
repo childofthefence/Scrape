7e11771900c81212cc7d671f142b7ffa9bb3c70a
michaelryan <michaeldryanii>
2018-09-04 12:44:51 -0400

    Initial commit

---
+++ venv/source/test.py
@@ -0,0 +1,26 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+import sys
+# Declare second integer, double, and String variables.
+intval=0
+doubleval = 0.0
+strval = "Zero"
+# Read and save an integer, double, and String to your variables.
+intval = int(sys.stdin.readline())
+doubleval = float(sys.stdin.readline())
+strval = sys.stdin.readlin()
+# Print the sum of both integer variables on a new line.
+newint = i + intval
+print(newint)
+# Print the sum of the double variables on a new line.
+
+#d = d + doubleval
+newdouble = d + doubleval
+print(newdouble)
+# Concatenate and print the String variables on a new line
+
+#s = s + strval
+print(s + strval)
+# The 's' variable above should be printed first.
